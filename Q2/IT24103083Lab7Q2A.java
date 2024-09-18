1896c155e1f04262d2e1a691b84b59f4200a1cbf
IT24103083 <it24103083@my.sliit.lk>
2024-09-18 10:16:19 +0530

    Add files via upload

---
+++ Q2/IT24103083Lab7Q2A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+   public class IT24103083Lab7Q2A{
+     public static void main(String[] args) {
+      
+     	 
+          int rows=4;
+          int columns=5;
+
+       for(int j=1; j<=rows;j++){
+          for(int i=1; i<=columns; i++){
+         System.out.print("$");
+       }
+       System.out.println();
+      }
+  }
+}
